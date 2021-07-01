31564e16c6e35f670f1d123723e024d6ff74a278
Miguel Hernandez <migher74@outlook.com>
2021-07-01 15:50:32 -0500

    Main menu created

---
+++ wallet.py
@@ -0,0 +1,9 @@
+# Menu del Programa
+
+print("Menu Principal")
+print("1. Recibir")
+print("2. Transferir")
+print("3. Balance Sencillo")
+print("4. Balance General")
+print("5. Historico de Transacciones")
+print("x. Salir")
